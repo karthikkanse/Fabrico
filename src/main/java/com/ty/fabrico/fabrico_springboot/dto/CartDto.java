1f15660f0302141887ff5e2adb6851c5953b884b
sasikalajavadeveloper <sasikalajavadeveloper@gmail.com>
2022-12-07 13:25:08 +0530

    cartDto added

---
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/CartDto.java
@@ -0,0 +1,32 @@
+package com.ty.fabrico.fabrico_springboot.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class CartDto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	private String name;
+	@NotNull
+	private int quantity;
+	@NotNull
+	private long phone;
+	@NotNull
+	private double totalcost;
+	
+	@OneToMany
+	List<ProductDto>product;
+
+}
