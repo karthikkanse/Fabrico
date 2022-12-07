8a661ad90faaebb87137d6754640fef62a4491c8
karthikjavadeveloper <karthikkanse002@gmail.com>
2022-12-07 16:00:40 +0530

    updated code

--- src/main/java/com/ty/fabrico/fabrico_springboot/repository/CustomerRepository.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/repository/CustomerRepository.java
@@ -4,13 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ty.fabrico.fabrico_springboot.dto.Customer;
 
-<<<<<<< HEAD
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
 	public Customer getCustomerByName(String email);
-=======
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-	public Customer getCustomerByEmail(String email);
->>>>>>> karthik
 }
