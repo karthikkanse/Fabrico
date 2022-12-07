142090e56d6983126ce14d232573df2294a69d0e
manoj kulli <sarupunit@gmail.com>
2022-12-07 12:35:35 +0530

    added completed waverDto

--- src/main/java/com/ty/fabrico/fabrico_springboot/dto/WeaverDto.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/WeaverDto.java
@@ -4,22 +4,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
-
-
-
+import lombok.Data;
 
 @Entity
-
+@Data
 public class WeaverDto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int weaverid;
+	@NotNull
 	private String weavername;
+	@NotNull
 	private String username;
+	@NotNull
 	private String password;
+	@NotNull
 	private long phone;
+	@NotNull
 	private String address;
 	
 }
