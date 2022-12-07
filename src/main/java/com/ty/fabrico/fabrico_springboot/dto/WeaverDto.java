27fade314b4f8861aa849c8e8f58429826a2edce
manoj kulli <sarupunit@gmail.com>
2022-12-07 12:25:19 +0530

    added Swager in pom.xml and modified waverDto

--- src/main/java/com/ty/fabrico/fabrico_springboot/dto/WeaverDto.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/WeaverDto.java
@@ -1,5 +1,25 @@
 package com.ty.fabrico.fabrico_springboot.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+
+
+@Entity
+
 public class WeaverDto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int weaverid;
+	private String weavername;
+	private String username;
+	private String password;
+	private long phone;
+	private String address;
+	
 }
