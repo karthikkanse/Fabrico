4adbfe5fe69a121868ee333eb4e3d898a8361603
karthikjavadeveloper <karthikkanse002@gmail.com>
2022-12-07 13:13:26 +0530

    Customer dto update new

--- src/main/java/com/ty/fabrico/fabrico_springboot/dto/CustomerDto.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/CustomerDto.java
@@ -4,11 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.GeneratorType;
-
+import javax.validation.constraints.NotNull;
 import lombok.Data;
-
 
 @Entity
 @Data
@@ -16,11 +13,14 @@ public class CustomerDto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int customerId;
-	
+	@NotNull
 	private String customerName;
+	@NotNull
 	private String email;
+	@NotNull
 	private String password;
+	@NotNull
 	private String address;
 	private String premium;
-	
+
 }
