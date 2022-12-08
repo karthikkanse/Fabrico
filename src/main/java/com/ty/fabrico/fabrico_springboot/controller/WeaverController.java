5adfea9f28a1619567cceee925936e9ca2414112
manoj kulli <sarupunit@gmail.com>
2022-12-08 18:19:54 +0530

    product service updated

--- src/main/java/com/ty/fabrico/fabrico_springboot/controller/WeaverController.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/controller/WeaverController.java
@@ -1,5 +1,27 @@
 package com.ty.fabrico.fabrico_springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.fabrico.fabrico_springboot.dto.Weaver;
+import com.ty.fabrico.fabrico_springboot.service.WeaverService;
+import com.ty.fabrico.fabrico_springboot.util.ResponseStructure;
+
+@RestController
+@RequestMapping("weaver")
 public class WeaverController {
 
+	@Autowired
+	WeaverService weaverService;
+	
+	@PostMapping
+	public ResponseEntity<ResponseStructure<Weaver>> saveWeaver(@RequestBody Weaver weaver) {
+		return weaverService.saveWeaver(weaver);
+	}
+	
+	
 }
--- src/main/java/com/ty/fabrico/fabrico_springboot/dto/Weaver.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/Weaver.java
@@ -2,6 +2,7 @@ package com.ty.fabrico.fabrico_springboot.dto;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -33,7 +34,7 @@ public class Weaver {
 	@NotNull
 	private String address;
 	
-	@OneToMany
+	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn
 	List<Product> product;
 	
