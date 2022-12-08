37b82ee5af64c85df4e07bbbda7bce0f0798bfad
umeshtiwarijavadeveloper <umeshtiwarijavadeveloper@gmail.com>
2022-12-08 11:54:16 +0530

    Exception Classes are Added

--- src/main/java/com/ty/fabrico/fabrico_springboot/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/exception/ApplicationExceptionHandler.java
@@ -1,5 +1,25 @@
 package com.ty.fabrico.fabrico_springboot.exception;
 
-public class ApplicationExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.ty.fabrico.fabrico_springboot.util.ResponseStructure;
+
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(NoSuchIdFoundException.class)
+	public  ResponseEntity<ResponseStructure<String>> noSuchIdFoundExceptionHandler(NoSuchIdFoundException exception){
+		
+		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+		ResponseEntity<ResponseStructure<String>> responseEntity;
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("No Such ID is Present");
+		responseStructure.setData(exception.getMessage());
+		return responseEntity =new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.NOT_FOUND);
+	}
 }
--- src/main/java/com/ty/fabrico/fabrico_springboot/exception/NoSuchIdFoundException.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/exception/NoSuchIdFoundException.java
@@ -1,5 +1,21 @@
 package com.ty.fabrico.fabrico_springboot.exception;
 
-public class NoSuchIdFoundException {
+public class NoSuchIdFoundException extends RuntimeException {
 
+private String message="No Such Id To be found in the Database";
+	
+	public NoSuchIdFoundException(String message) {
+		super();
+		this.message=message;
+	}
+	
+	public NoSuchIdFoundException() {
+		super();
+	}
+	
+	@Override
+	public String getMessage()
+	{
+		return message;
+	}
 }
