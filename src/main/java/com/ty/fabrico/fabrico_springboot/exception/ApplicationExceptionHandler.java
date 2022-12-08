ed33944952aacd22c12edfe645009ea3a177bb76
manoj kulli <sarupunit@gmail.com>
2022-12-08 14:20:32 +0530

    Exception classes and weaver service added

--- src/main/java/com/ty/fabrico/fabrico_springboot/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/exception/ApplicationExceptionHandler.java
@@ -8,18 +8,39 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import com.ty.fabrico.fabrico_springboot.util.ResponseStructure;
 
-
 @RestControllerAdvice
-public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler{
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(NoSuchIdFoundException.class)
-	public  ResponseEntity<ResponseStructure<String>> noSuchIdFoundExceptionHandler(NoSuchIdFoundException exception){
-		
-		ResponseStructure<String> responseStructure=new ResponseStructure<String>();
+	public ResponseEntity<ResponseStructure<String>> noSuchIdFoundExceptionHandler(NoSuchIdFoundException exception) {
+
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
 		ResponseEntity<ResponseStructure<String>> responseEntity;
 		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
 		responseStructure.setMessage("No Such ID is Present");
 		responseStructure.setData(exception.getMessage());
-		return responseEntity =new ResponseEntity<ResponseStructure<String>>(responseStructure,HttpStatus.NOT_FOUND);
+		return responseEntity = new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(NoSuchUsernameFoundException.class)
+	public ResponseEntity<ResponseStructure<String>> noSuchUsernameFoundExceptionHandler(NoSuchUsernameFoundException exception) {
+
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
+		ResponseEntity<ResponseStructure<String>> responseEntity;
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("User Name Not Found");
+		responseStructure.setData(exception.getMessage());
+		return responseEntity = new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(PasswordIncorrectException.class)
+	public ResponseEntity<ResponseStructure<String>> passwordIncorrectExceptionHandler(PasswordIncorrectException exception) {
+
+		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
+		ResponseEntity<ResponseStructure<String>> responseEntity;
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("Invalid Password");
+		responseStructure.setData(exception.getMessage());
+		return responseEntity = new ResponseEntity<ResponseStructure<String>>(responseStructure, HttpStatus.NOT_FOUND);
 	}
 }
