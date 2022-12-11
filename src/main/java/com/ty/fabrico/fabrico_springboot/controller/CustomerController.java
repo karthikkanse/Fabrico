2b9b568f917cb52613418b385b524672619f7199
umeshtiwarijavadeveloper <umeshtiwarijavadeveloper@gmail.com>
2022-12-11 21:46:12 +0530

    Changes Uploaded

--- src/main/java/com/ty/fabrico/fabrico_springboot/controller/CustomerController.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/controller/CustomerController.java
@@ -1,7 +1,6 @@
 package com.ty.fabrico.fabrico_springboot.controller;
-
-import org.mapstruct.ValueMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ty.fabrico.fabrico_springboot.dto.Customer;
 import com.ty.fabrico.fabrico_springboot.service.CustomerService;
 import com.ty.fabrico.fabrico_springboot.util.ResponseStructure;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("customer")
@@ -24,23 +25,52 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 	
-	@PostMapping
+	@ApiOperation(value="Save Customer" , notes="It is used to save the Customer")
+	@ApiResponses(value= {@ApiResponse(code=201, message="Created"),
+			@ApiResponse(code=500, message="Internal Server Error"),
+			@ApiResponse(code=404, message="Not Found")})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces= {
+		MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<ResponseStructure<Customer>> saveCustomer(@RequestBody Customer customer) {
 		return customerService.saveCustomer(customer);
 	}
-	@PutMapping
+	
+	@ApiOperation(value="Update Customer" , notes="It is used to update the Customer")
+	@ApiResponses(value= {@ApiResponse(code=201, message="Created"),
+			@ApiResponse(code=500, message="Internal Server Error"),
+			@ApiResponse(code=404, message="Not Found")})
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces= {
+		MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<ResponseStructure<Customer>> updateCustomer(@RequestBody Customer customer,@RequestParam int customerId) {
 		return customerService.updateCustomer(customer,customerId);
 	}
-	@GetMapping
+	
+	@ApiOperation(value="Fetch Customer by Id" , notes="It is used to fetch the Customer by Id")
+	@ApiResponses(value= {@ApiResponse(code=201, message="Created"),
+			@ApiResponse(code=500, message="Internal Server Error"),
+			@ApiResponse(code=404, message="Not Found")})
+	@GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces= {
+		MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<ResponseStructure<Customer>> getCustomerById(@RequestParam int customerId) {
 		return customerService.getCustomerById(customerId);
 	}
-	@PatchMapping
+	
+	@ApiOperation(value="Fetch Customer by Email" , notes="It is used to fetch the Customer by Email")
+	@ApiResponses(value= {@ApiResponse(code=201, message="Created"),
+			@ApiResponse(code=500, message="Internal Server Error"),
+			@ApiResponse(code=404, message="Not Found")})
+	@PatchMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces= {
+		MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<ResponseStructure<Customer>> getCustomerByEmail(@RequestBody Customer customer) {
 		return customerService.getCustomerByEmail(customer);
 	}
-	@DeleteMapping
+	
+	@ApiOperation(value="Delete Customer" , notes="It is used to delete the Customer")
+	@ApiResponses(value= {@ApiResponse(code=201, message="Created"),
+			@ApiResponse(code=500, message="Internal Server Error"),
+			@ApiResponse(code=404, message="Not Found")})
+	@DeleteMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces= {
+		MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<ResponseStructure<Customer>> deleteCustomer(@RequestParam int customerId) {
 		return customerService.deleteCustomer(customerId);
 	}
