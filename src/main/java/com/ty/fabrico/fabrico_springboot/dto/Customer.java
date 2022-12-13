d4acc5e33e8b253784ce743a768b071a20cc146d
sasikalajavadeveloper <sasikalajavadeveloper@gmail.com>
2022-12-13 11:35:28 +0530

    file

--- src/main/java/com/ty/fabrico/fabrico_springboot/dto/Customer.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/dto/Customer.java
@@ -1,6 +1,5 @@
 package com.ty.fabrico.fabrico_springboot.dto;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
