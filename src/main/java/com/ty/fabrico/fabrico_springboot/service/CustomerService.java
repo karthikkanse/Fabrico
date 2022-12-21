b2b778405f2a979a83c322e2aaa907da3686eb40
karthikjavadeveloper <karthikkanse002@gmail.com>
2022-12-21 14:54:55 +0530

    log4j implemented in customer module

--- src/main/java/com/ty/fabrico/fabrico_springboot/service/CustomerService.java
+++ src/main/java/com/ty/fabrico/fabrico_springboot/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.ty.fabrico.fabrico_springboot.service;
 
 import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,14 @@ import com.ty.fabrico.fabrico_springboot.exception.NoSuchUsernameFoundException;
 import com.ty.fabrico.fabrico_springboot.exception.PasswordIncorrectException;
 import com.ty.fabrico.fabrico_springboot.util.ResponseStructure;
 
+
 @Service
 public class CustomerService {
 
 	@Autowired
 	private CustomerDao customerDao;
+	
+	private static final Logger LOGGER=Logger.getLogger(CustomerService.class);
 	
 	public ResponseEntity<ResponseStructure<Customer>> saveCustomer(Customer customer) {
 		ResponseStructure<Customer> responseStructure = new ResponseStructure<Customer>();
@@ -26,6 +30,7 @@ public class CustomerService {
 		responseStructure.setStatus(HttpStatus.CREATED.value());
 		responseStructure.setMessage("Saved");
 		responseStructure.setData(customerDao.saveCustomer(customer));
+		LOGGER.debug("Customer Saved");
 		return responseEntity = new ResponseEntity<ResponseStructure<Customer>>(responseStructure, HttpStatus.CREATED);
 	}
 
@@ -38,8 +43,10 @@ public class CustomerService {
 			responseStructure.setStatus(HttpStatus.OK.value());
 			responseStructure.setMessage("Updated");
 			responseStructure.setData(customerDao.updateCustomer(customer));
+			LOGGER.debug("Customer Updated");
 			return responseEntity = new ResponseEntity<ResponseStructure<Customer>>(responseStructure, HttpStatus.OK);
 		} else
+			LOGGER.error("Customer Not Found to Update");
 			throw new NoSuchIdFoundException("No Id Found to Update");
 	}
 
@@ -51,8 +58,10 @@ public class CustomerService {
 			responseStructure.setStatus(HttpStatus.OK.value());
 			responseStructure.setMessage("Found");
 			responseStructure.setData(optional.get());
+			LOGGER.debug("Customer Found");
 			return responseEntity = new ResponseEntity<ResponseStructure<Customer>>(responseStructure, HttpStatus.OK);
 		} else
+			LOGGER.error("Customer Not Found");
 			throw new NoSuchIdFoundException("No such Id is Found");
 
 		}
@@ -68,11 +77,14 @@ public class CustomerService {
 				responseStructure.setStatus(HttpStatus.OK.value());
 				responseStructure.setMessage("Login Successful as Customer");
 				responseStructure.setData(customer1);
+				LOGGER.debug("Login Successfull");
 				return responseEntity = new ResponseEntity<ResponseStructure<Customer>>(responseStructure,
 						HttpStatus.OK);
 			} else
+				LOGGER.error("Invalid Password");
 				throw new PasswordIncorrectException("Invalid Password");
 		} else
+			LOGGER.error("Invalid Mail-Id");
 			throw new NoSuchUsernameFoundException("Email Not Found");
 
 	}
@@ -86,8 +98,10 @@ public class CustomerService {
 			responseStructure.setStatus(HttpStatus.OK.value());
 			responseStructure.setMessage("Deleted");
 			responseStructure.setData(optional.get());
+			LOGGER.debug("Customer Deleted");
 			return responseEntity = new ResponseEntity<ResponseStructure<Customer>>(responseStructure, HttpStatus.OK);
 		} else
+			LOGGER.error("Customer Not Found to Delete");
 			throw new NoSuchIdFoundException("No Id found to Delete");
 	}
 }
